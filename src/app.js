d6a1c92b3cddcea8ba40e8550644c0028b43bb24
Cyb3rV <152754161+Cyb3rV@users.noreply.github.com>
2024-02-10 01:21:58 +0000

    Finished project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+let domains = [];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i of pronoun)
+    for (let j of adj)
+      for (let k of noun) {
+        domains.push(i + j + k + ".com");
+      }
+
+  document.querySelector("div").innerHTML = domains.join("<br/>");
 };
